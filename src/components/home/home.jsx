53d41421143fc901755e9ed89773236f88a546aa
KARAN KUMAR <kkvmalhi@gmail.com>
2023-09-05 19:53:22 +0500

    deployed on firebase

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -1,14 +1,55 @@
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import axios from "axios";
 import WeatherCard from "../weatherCard/weatherCard.jsx";
+import * as Icon from "react-bootstrap-icons";
+
 import "./home.css";
+import AppIcon from "../images/AppIcon.png";
 
 const Home = () => {
   //-------State for Weather Data-------//
+  // const [weatherData, setWeatherData] = useState([]);
   const [weatherData, setWeatherData] = useState(null);
 
   // Getting Value From input in react using useRef
   const cityNameInput = useRef(null);
+
+  // const [isLoading, setIsLoading] = useState(false)
+
+  // const [currentLocationWeather, setCurrentLocationWeather] = useState(null);
+  //-----Function to get the current location of User onload------//
+  useEffect(() => {
+    //--- AbortController for aborting the request if user closes the tab or browser---//
+    const controller = new AbortController();
+    //--Checking the location if found--//
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(async (location) => {
+        try {
+          // setIsLoading(true);         //--Instead of city name gating weather by lon & lat--//
+          let API_KEY = "bd30503813924350feea847416d7bff9";
+          const response = await axios.get(
+            `https://api.openweathermap.org/data/2.5/weather?lat=${location.coords.latitude}&lon=${location.coords.longitude}&appid=${API_KEY}&units=metric`,
+            {
+              signal: controller.signal,
+            }
+          );
+          console.log(response.data);
+          setWeatherData(response.data);
+          // setIsLoading(false);         //--Instead of city name gating weather by lon & lat--//
+          // setCurrentLocationWeather(response.data);
+        } catch (error) {
+          console.log(error.data);
+          // setIsLoading(false);         //--Instead of city name gating weather by lon & lat--//
+        }
+      });
+    } else {
+      console.log("Geolocation is not supported by this browser.");
+    }
+
+    return () => {
+      controller.abort(); //Abort the request if user closes the tab or browser
+    };
+  }, []);
 
   //---------- Weather Handler Function-----------//
   const weatherHandler = async (e) => {
@@ -19,18 +60,22 @@ const Home = () => {
     let API_KEY = "bd30503813924350feea847416d7bff9";
     try {
       const response = await axios.get(
-        `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${API_KEY}&units=metric`
+        `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${API_KEY}&units=metric`,
       );
       console.log(response.data);
       setWeatherData(response.data);
     } catch (error) {
       console.log(error.data);
+      alert("City Not Found");
+      window.location.reload();
     }
   };
 
   return (
     <div className="main">
       <form onSubmit={weatherHandler}>
+        <img src={AppIcon} alt="App Icon" />
+        <h1>Weather App</h1>
         <div>
           <input
             type="text"
@@ -43,16 +88,43 @@ const Home = () => {
           />
         </div>
         <div>
-          <button type="submit">Get Weather</button>
+          <button type="submit">
+            Get Weather <Icon.Search />
+          </button>
+        </div>
+        <div>
+          <p>
+            Developed By:{" "}
+            <a
+              href="https://github.com/KaranKumar05/weather-app.git"
+              target="_blank"
+            >
+              Karan Kumar
+            </a>
+          </p>
+          {/* Todo: History Component that Hold tha previous search of the user */}
         </div>
       </form>
       <div>
+        {/* {isLoading? <div className="noDataContainer">Loading....</div>: null} */}
+        {/* {isLoading? null: <div className="noDataContainer">No Data</div>} */}
         {/* Checking if Data is Available Then Render The Card Otherwise "No Data" */}
+        {/* {(weatherData.length || currentLocationWeather) ? (
+          null
+        ) : (
+          <div className="noDataContainer">No Data</div>
+        )} */}
+
         {weatherData ? (
           <WeatherCard weatherData={weatherData} />
         ) : (
-          <div className="noDataContainer">No Data</div>
+          <div className="noDataContainer">Loading....</div>
         )}
+        {/* {!weatherData || isLoading ? <div className="noDataContainer">No Data</div>: null} */}
+        {/* {weatherData ? <WeatherCard weatherData={weatherData} /> : null} */}
+        {/* {currentLocationWeather ? (
+          <WeatherCard weatherData={currentLocationWeather} />
+        ) : null} */}
       </div>
     </div>
   );
--- src/components/weatherCard/weatherCard.jsx
+++ src/components/weatherCard/weatherCard.jsx
@@ -1,22 +1,82 @@
 import "./weatherCard.css";
+import ClearSky from "../images/clearSky.png";
+import Cloudy from "../images/cloudy.png";
+import Rain from "../images/rain.png";
+import MostlyCloudy from "../images/mostlyCloudy.png";
+import Snow from "../images/snow.png";
+import * as Icon from "react-bootstrap-icons";
+
 
 const WeatherCard = ({ weatherData }) => {
+  let weatherImgData = weatherData?.weather?.[0]?.main;
+  console.log(weatherImgData);
+
+  let weatherImg = ClearSky;
+  if (weatherImgData === "Snow") {
+    weatherImg = Snow;
+  } else if (weatherImgData === "Rain") {
+    weatherImg = Rain;
+  } else if (weatherImgData === "Clouds") {
+    weatherImg = Cloudy;
+  } else if (weatherImgData === "Mostly Clouds") {
+    weatherImg = MostlyCloudy;
+  }
+
+  const currentDate = new Date();
+
+  const fullDate = currentDate.toLocaleDateString("en-US", {
+    day: "numeric",
+    month: "short",
+  });
+
+  const currentTime = currentDate.toLocaleTimeString("en-US", {
+    hour: "numeric",
+    minute: "numeric",
+    hour12: true,
+  });
+
+  const monthName = currentDate.toLocaleDateString("en-US", { month: "short" });
+  console.log(fullDate, currentTime);
+  let date = `${fullDate}`;
+  let time = `${currentTime}`;
+
+
   return (
     <div className="container">
       <div>
-        <span className="cityName">{weatherData?.name}</span>
-        <span className="counteryName">{weatherData?.sys?.country}</span>
+        <div>
+          <span>{weatherData?.name}</span>
+          <span>{weatherData?.sys?.country}</span>
+        </div>
+        <div>
+          <p><Icon.Calendar />{date}</p>
+          <p><Icon.Clock />{time}</p>
+        </div>
       </div>
 
       <div>
-        <div><span>{weatherData?.main?.temp}</span>°C</div>
-        <div>{weatherData?.weather?.[0]?.main}</div>
+        <div className="weatherIconContainer">
+          <div>{weatherData?.weather?.[0]?.main}</div>
+          <div className="iconImg">
+            <img src={weatherImg} alt="App Icon" width={100} height={100} />
+          </div>
+        </div>
+
+        <div className="mainDataContainer">
+          <div>
+            <span>{weatherData?.main?.temp}</span>°C
+          </div>
+          <div className="minMaxTempContainer">
+            <span><Icon.Thermometer />Min:{weatherData?.main?.temp_min}<span>°C</span></span>
+            <span><Icon.ThermometerHigh />Max:{weatherData?.main?.temp_max}<span>°C</span></span>
+          </div>
+        </div>
       </div>
 
       <div>
-        <span>Pressure: {weatherData?.main?.pressure}</span>
-        <span>Humidity: {weatherData?.main?.humidity}</span>
-        <span>Wind: {weatherData?.wind?.speed}</span>
+        <span><Icon.Compass /> Pressure: {weatherData?.main?.pressure} Pa</span>
+        <span><Icon.Moisture /> Humidity: {weatherData?.main?.humidity} %</span>
+        <span><Icon.Wind /> Wind: {weatherData?.wind?.speed} km/h</span>
       </div>
     </div>
   );
